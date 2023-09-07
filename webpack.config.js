7eb137d958a70746d6cf32ef686a84167b682e2d
Patrick Nilan <nilan.patrick@gmail.com>
2023-09-07 12:22:46 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.jsx'),
+  output: {
+    path: path.join(__dirname, '/public/'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-flow-test',
+      template: path.join(__dirname, '/src/assets/index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.sass$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      }
+    ]
+  }
+};
+
